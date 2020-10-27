417447476d40279c1bc4907881a6a03697b945d7
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2020-10-27 15:12:57 +0300

    Styling fixes with prettier

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 let config = {
-    dbUrl: process.env.DBURL || "mongodb+srv://Hanna:1q2w3e4r@cluster0.d39nt.mongodb.net/Art-Gallery",
-    port: process.env.PORT || 3010,
-    env: process.env.NODE_ENV || "development"
+  dbUrl:
+    process.env.DBURL ||
+    "mongodb+srv://admin:helloeverybody@cluster0.jovv7.mongodb.net/Art-Gallery",
+  port: process.env.PORT || 3010,
+  env: process.env.NODE_ENV || "development",
 };
 
 module.exports = config;
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,67 +1,65 @@
-const bodyParser = require( "body-parser" );
-const express = require( "express" );
-const morgan = require( "morgan" );
-const path = require( "path" );
-const routes = require( "../routes" );
-const compression = require( "compression" );
+const bodyParser = require("body-parser");
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const routes = require("../routes");
+const compression = require("compression");
 // const logger = require( "../services/Logger" );
-const config = require( "../config" );
+const config = require("../config");
 
 class ExpressLoader {
-    constructor () {
-        const app = express();
-        console.log('1');
-        app.use( ExpressLoader.errorHandler );
-        console.log('2');
-        app.use( express.static( path.join( __dirname, "uploads" ) ) );
-        console.log('3');
+  constructor() {
+    const app = express();
 
-        app.use( morgan( "dev" ) );
-        app.use( compression() );
-        app.use( bodyParser.urlencoded( {
-            extended: false,
-            limit: "20mb"
-        } ) );
-        app.use( bodyParser.json( { limit: "20mb" } ) );
+    app.use(ExpressLoader.errorHandler);
+    app.use(express.static(path.join(__dirname, "uploads")));
 
-        routes( app );
+    app.use(morgan("dev"));
+    app.use(compression());
+    app.use(
+      bodyParser.urlencoded({
+        extended: false,
+        limit: "20mb",
+      })
+    );
+    app.use(bodyParser.json({ limit: "20mb" }));
 
+    routes(app);
 
-        this.server = app.listen( config.port, () => {
-            // logger.info( `Express running, now listening on port ${config.port}` );
-            console.log('Express running');
-        } );
+    this.server = app.listen(config.port, () => {
+      console.log(`Express running, now listening on port ${config.port}`);
+    });
+  }
+
+  get Server() {
+    return this.server;
+  }
+
+  static errorHandler(error, req, res, next) {
+    let parsedError;
+
+    try {
+      if (error && typeof error === "object") {
+        parsedError = JSON.stringify(error);
+      } else {
+        parsedError = error;
+      }
+    } catch (e) {
+      console.log(e);
+      // logger.error( e );
     }
 
-    get Server () {
-        return this.server;
+    // logger.error( parsedError );
+
+    if (res.headersSent) {
+      return next(error);
     }
 
-   static errorHandler ( error, req, res, next ) {
-        let parsedError;
-
-        try {
-            if ( error && typeof error === "object" ) {
-                parsedError = JSON.stringify( error );
-            } else {
-                parsedError = error;
-            }
-        } catch ( e ) {
-            console.log(e);
-            // logger.error( e );
-        }
-
-        // logger.error( parsedError );
-
-        if ( res.headersSent ) {
-            return next( error );
-        }
-
-        res.status( 400 ).json( {
-            success: false,
-            error
-        } );
-    }
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
 }
 
 module.exports = ExpressLoader;
