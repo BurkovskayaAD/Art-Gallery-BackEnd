452868fe7232361808ba4c0e4578f8516d9ad611
BurkovskayaAD <hanna.burkovskaya@gmail.com>
2020-11-13 19:49:49 +0300

    Update schema for exhibitions

--- src/models/exhibitions.js
+++ src/models/exhibitions.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const exhibitionSchema = new Schema({
     name: String,
-    date: {type: Date},
+    dateStart: Date,
+    dateEnd: Date,
     about: String,
     poster: String,
     lastModified: { type: Date, default: Date.now },
