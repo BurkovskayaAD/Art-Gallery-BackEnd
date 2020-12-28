b3dd99520a593de3c8bcb4be351e80f64a62051c
BurkovskayaAD <hanna.burkovskaya@gmail.com>
2020-12-28 13:29:18 +0300

    Update request

--- src/routes/artistsRoute.js
+++ src/routes/artistsRoute.js
@@ -62,7 +62,7 @@ router.delete('/:artistIdDelete', async (req, res) => {
 });
 
 router.post('/:artistEditId', async (req, res) => {
-  const artistEdit = await artistService.findArtistByIdAndUpdate(req.params.artistEditId);
+  const artistEdit = await artistService.findArtistByIdAndUpdate(req.params.artistEditId, {$set: req.body});
   if (artistEdit.errorPresent) {
     res.status(500).json(artistEdit.error);
   } else {
